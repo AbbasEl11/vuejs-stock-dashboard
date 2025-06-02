c94ecf372483a93211b81de9fca427a81dc5de77
Abbas El <abbas-el-mahmoud@hotmail.com>
2025-06-02 16:52:59 +0200

    first component created

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
-
-  </div>
+    <BaseCard>
+      <h1>Überschrift</h1>
+      <p></p>
+    </BaseCard>
 </template>
 
 <script>
-
+import BaseCard from './components/BaseCard.vue';
 
 export default {
   name: 'App',
   components: {
+    BaseCard
   }
 }
 </script>
@@ -17,11 +19,12 @@ export default {
 <style>
 body {
   margin: 0;
-  padding: 0;
 }
 #app {
+  width: 100vw;
+  height: 100vh;
+  padding: 150px;
   background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
-    width: 100vw;
-    height: 100vh;
+  box-sizing: border-box;
 }
 </style>
